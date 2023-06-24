e5f6f645c0c8958151b3833cc521aa530706550e
planb-yano <takahiro.yano@plan-b.co.jp>
2023-06-24 14:19:26 +0900

    NotFoundを追加

---
+++ src/pages/notfound/NotFound.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./NotFound.scss"
+
+const NotFound = () => {
+  return (
+    <div className="notFound">
+      NotFoundです。
+      <br />
+      メニューからサイトに戻ってください。
+    </div>
+  );
+};
+
+export default NotFound;
