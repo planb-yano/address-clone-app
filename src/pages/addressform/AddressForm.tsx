43d9bdffe9802dc4e09aad6550495113b65663c5
planb-yano <takahiro.yano@plan-b.co.jp>
2023-07-03 14:33:56 +0900

    フォームの入力値を管理できるようにする

--- src/pages/addressform/AddressForm.tsx
+++ src/pages/addressform/AddressForm.tsx
@@ -2,54 +2,92 @@ import React, { useState } from "react";
 import "./AddressForm.scss";
 import { Link } from "react-router-dom";
 
+interface FormData {
+  name: string;
+  tel: string;
+  email: string;
+  address: string;
+}
+
 const Address = () => {
   const [nameActive, setNameActive] = useState<boolean>(false);
   const [telActive, setTelActive] = useState<boolean>(false);
   const [emailActive, setEmailActive] = useState<boolean>(false);
   const [addressActive, setAddressActive] = useState<boolean>(false);
+  const [formData, setFormData] = useState<FormData>({
+    name: "",
+    tel: "",
+    email: "",
+    address: "",
+  });
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
   return (
     <div className="addressForm">
       <h2 className="addressFromTitle">連絡先編集</h2>
       <div className="addressFormContainer">
         <form>
           <div className={`addressFormName ${nameActive ? "isActive" : ""}`}>
-            <label htmlFor="name">名前</label>
+            <label className={formData.name ? "valid" : ""} htmlFor="name">
+              名前
+            </label>
             <input
               type="text"
               id="name"
               name="name"
               onFocus={() => setNameActive(true)}
               onBlur={() => setNameActive(false)}
+              onChange={(e) => handleInputChange(e)}
+              value={formData.name}
             />
           </div>
           <div className={`addressFormTel ${telActive ? "isActive" : ""}`}>
-            <label htmlFor="tel">電話番号</label>
+            <label className={formData.tel ? "valid" : ""} htmlFor="tel">
+              電話番号
+            </label>
             <input
               type="text"
               id="tel"
               name="tel"
               onFocus={() => setTelActive(true)}
               onBlur={() => setTelActive(false)}
+              onChange={(e) => handleInputChange(e)}
+              value={formData.tel}
             />
           </div>
           <div className={`addressFormEmail ${emailActive ? "isActive" : ""}`}>
-            <label htmlFor="email">メールアドレス</label>
+            <label className={formData.email ? "valid" : ""} htmlFor="email">
+              メールアドレス
+            </label>
             <input
               type="text"
               id="email"
               name="email"
               onFocus={() => setEmailActive(true)}
               onBlur={() => setEmailActive(false)}
+              onChange={(e) => handleInputChange(e)}
+              value={formData.email}
             />
           </div>
-          <div className={`addressFormAddress ${addressActive ? "isActive" : ""}`}>
-            <label htmlFor="address">住所</label>
+          <div
+            className={`addressFormAddress ${addressActive ? "isActive" : ""}`}
+          >
+            <label
+              className={formData.address ? "valid" : ""}
+              htmlFor="address"
+            >
+              住所
+            </label>
             <input
               type="text"
               id="address"
               name="address"
               onFocus={() => setAddressActive(true)}
               onBlur={() => setAddressActive(false)}
+              onChange={(e) => handleInputChange(e)}
+              value={formData.address}
             />
           </div>
         </form>
